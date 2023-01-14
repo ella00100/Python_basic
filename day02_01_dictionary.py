e470dc627c6748ee357997bd481ea805fba397eb
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-01-14 10:38:39 +0900

    Add files via upload

---
+++ day02_01_dictionary.py
@@ -0,0 +1,13 @@
+harry_poter={'헤르미온느':'그리핀도르',
+             '말포이': '슬리데린',
+             '말포이':'그리핀도르'}
+print(harry_poter)
+print(harry_poter['말포이']) #dictionary는 key:value = 1:1
+print(type(harry_poter))
+
+harry_poter={'헤르미온느':'그리핀도르',
+             '말포이': '슬리데린',
+             '론':'그리핀도르'}
+print(harry_poter)
+print(type(harry_poter))
+
