f2abc70e301cda61a1f5a566e030c6381fefe869
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-01-14 10:50:33 +0900

    Add files via upload

---
+++ day02_prob02.py
@@ -0,0 +1,13 @@
+small = True
+green = False
+
+if small:
+    if green:
+        print("완두콩")
+    else:
+        print('체리')
+else:
+    if green:
+        print('수박')
+    else:
+        print('호박')
