c8d6b5b98036714baa73ce66e8405562bacdabbd
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-01-14 10:29:32 +0900

    Add files via upload

---
+++ day01_02_countdown.py
@@ -0,0 +1,13 @@
+#교재 countdown 예제
+
+for countdown in 5, 4, 3, 2, 1, "hey!" :
+    print(countdown)
+print('프로그램 종료')
+
+countdown_list = [5, 4, 3, 2, 1, "hey!"]
+for countdown in countdown_list :
+    print(countdown)
+print('프로그램 종료')
+
+print(countdown_list[3])
+print('프로그램 종료')
