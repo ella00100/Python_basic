ce065ed8ac1c7fc907d7014d6663e8db646246d3
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-01-14 10:47:30 +0900

    Add files via upload

---
+++ day02_04_code.py
@@ -0,0 +1,17 @@
+#2진수 = 0b, 16진수 = 0x, 8진수 = 0o
+number = 0b10011010
+print(number)
+number = 0x9a
+print(number)
+number = 0o232
+print(number)
+
+#2진수 = bin , 16진수 = hex, 8진수 = oct
+value=154
+print(bin(value))
+print(hex(value))
+print(oct(value))
+
+#ord는 ascii code
+print(ord(" "))
+print(hex(ord(" ")))
