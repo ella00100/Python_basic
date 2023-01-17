5975a432be1050d36f96bca7c6fecc3b072030d5
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-01-17 13:40:26 +0900

    Add files via upload

---
+++ day04_04_for_in.py
@@ -0,0 +1,6 @@
+rows =range(1,4)
+cols = range(1,3)
+
+cells = [(row,col) for row in rows for col in cols]
+for cell in cells:
+    print(cell)
---
+++ day04_05_range.py
@@ -0,0 +1,13 @@
+#list comprehension
+
+odd_lists = []
+for i in range(1,11):
+    if i % 2 == 1:
+        odd_lists.append(i)
+print(odd_lists)
+
+odd_lists = [i for i in range(1,11) if i % 2 == 1]
+print(odd_lists, type(odd_lists))
+
+odd_tuples = (i for i in range(1,11) if i % 2 == 1)
+print(odd_tuples, type(odd_tuples))
